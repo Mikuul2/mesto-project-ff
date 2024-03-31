c48c176d2f8dfb2b02917d131fdf1635de6d2022
MiKuul <89446067+MiKuul@users.noreply.github.com>
2024-03-31 17:57:33 +0300

    createCard function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const container = document.querySelector('.content');
+const cardsList = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCards(cards) {
+  cards.forEach(function (element) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
+      deleteCard (cardElement);
+    });
+    cardsList.append(cardElement)
+  })
+}
 
 // @todo: Функция удаления карточки
+function deleteCard (element) {
+  element.remove();
+}
 
 // @todo: Вывести карточки на страницу
+createCards (initialCards);
